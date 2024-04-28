b808fcd3049a82c01c1b15e118b255bdbf3be84f
Christina <christina.m.wilson44@gmail.com>
2024-04-28 15:24:30 +0100

    added step counter

--- app/src/main/java/com/example/diabout/activities/RecordActivity.kt
+++ app/src/main/java/com/example/diabout/activities/RecordActivity.kt
@@ -3,11 +3,18 @@ package com.example.diabout.activities
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.TimePicker
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -21,11 +28,16 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import kotlin.math.truncate
 
-class RecordActivity : ComponentActivity() {
+class RecordActivity : ComponentActivity(), SensorEventListener {
     lateinit var dbHandler : UserDBHelper
     lateinit var stepsText : EditText
     lateinit var timePicker : TimePicker
     lateinit var datePicker: DatePicker
+    lateinit var startButton : Button
+    lateinit var stopButton : Button
+    var countSteps : Boolean = false
+    var totalSteps : Float = 0.0f
+    var previousSteps : Float = 0.0f
 
     private fun checkInput(value: String): Boolean {
         return if (value.length >0)
@@ -33,13 +45,22 @@ class RecordActivity : ComponentActivity() {
         else
             false
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_activity)
 
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val stepSensor = sensorManager.getDefaultSensor((Sensor.TYPE_STEP_COUNTER))
+
         stepsText = findViewById(R.id.stepsInput)
         timePicker= findViewById(R.id.timePicker)
         datePicker = findViewById(R.id.datePicker)
+
+        startButton = findViewById(R.id.startStepCounter)
+        stopButton = findViewById(R.id.stopStepCounter)
+
+        stopButton.visibility = View.INVISIBLE
 
         dbHandler = UserDBHelper(this)
 
@@ -47,6 +68,14 @@ class RecordActivity : ComponentActivity() {
         val userID = sharedPreferences.getString("ID", "0")
 //        val intent = intent
 //        val userID = intent.getStringExtra("ID")
+
+        previousSteps = sharedPreferences.getFloat("previousSteps", 0f)
+
+        if (stepSensor == null) {
+            Toast.makeText(this, "This device does not have the sensors needed to detect steps", Toast.LENGTH_LONG).show()
+        } else {
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        }
 
         val backButton = findViewById<ImageButton>(R.id.backButton)
         backButton.setOnClickListener {
@@ -84,7 +113,38 @@ class RecordActivity : ComponentActivity() {
             val alertDialog = alertBuilder.create()
             alertDialog.show()
         }
+
+        startButton.setOnClickListener{
+            println("start clicked")
+            countSteps = true
+            previousSteps = sharedPreferences.getFloat("previousSteps", 0f)
+            startButton.visibility = View.INVISIBLE
+            stopButton.visibility = View.VISIBLE
+        }
+        stopButton.setOnClickListener{
+            println("stop clicked")
+            addAutomaticActivity(userID!!)
+            previousSteps = totalSteps
+            val editor = sharedPreferences.edit()
+            editor.putFloat("previousSteps", previousSteps)
+            editor.apply()
+            countSteps = false
+            Toast.makeText(this, "Activity added", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Dashboard::class.java)
+            intent.putExtra("ID", userID)
+            startActivity(intent)
+            finish()
+        }
     }
+
+    private fun addAutomaticActivity(userID: String){
+        val now = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm")
+        val time = formatter.format(now)
+        val record = RecordItem(0, userID.toInt(), 2, time, totalSteps.toInt() - previousSteps.toInt())
+        dbHandler.addRecord(record)
+    }
+
     private fun addActivity(userID : String, value : Int) {
         var year = datePicker.year.toString()
         var month = (datePicker.month+1).toString()
@@ -108,6 +168,19 @@ class RecordActivity : ComponentActivity() {
         val time = "$year-$month-$day $hour:$minute"
         val record = RecordItem(0, userID.toInt(), 2, time, value)
         dbHandler.addRecord(record)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val stepsText = findViewById<TextView>(R.id.totalSteps)
+        if (countSteps) {
+            totalSteps = event!!.values[0]
+            val steps = totalSteps.toInt() - previousSteps.toInt()
+            stepsText.text = steps.toString() + " Steps"
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        println("Accuracy has changed to "+ accuracy)
     }
 
 }
